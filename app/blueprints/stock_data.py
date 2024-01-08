fa2260bbabca026e1ad9f1e4e85253c0872df93a
ZengChao <42760076+Neekky@users.noreply.github.com>
2024-01-08 16:56:47 +0800

    Update stock_data.py

--- app/blueprints/stock_data.py
+++ app/blueprints/stock_data.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 from app.models.stock_data import StockData
 
-stock_data_bp = Blueprint('stock_data', __name__, , url_prefix='/stock_data')
+stock_data_bp = Blueprint('stock_data', __name__, url_prefix='/stock_data')
 
 @stock_data_bp.route('/')
 def index():
