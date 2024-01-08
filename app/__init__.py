ba63ca93183ef44d0d4b209eda178603065d3c5e
ZengChao <42760076+Neekky@users.noreply.github.com>
2024-01-08 16:51:26 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,20 @@
 from flask import Flask
-from config import Config
+# from config import Config
 from .blueprints.main import main_bp
 from .blueprints.stock_data import stock_data_bp
 from flask_cors import CORS
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(config_class)
+    # app.config.from_object(config_class)
     CORS(app)
 
     # 初始化数据库
-    from app.models import db
-    db.init_app(app)
+    # from app.models import db
+    # db.init_app(app)
     
     # 注册蓝图
-    app.register_blueprint(main_bp)
-    app.register_blueprint(stock_data_bp, url_prefix='/auth')
+    # app.register_blueprint(main_bp)
+    # app.register_blueprint(stock_data_bp, url_prefix='/auth')
     
     return app
