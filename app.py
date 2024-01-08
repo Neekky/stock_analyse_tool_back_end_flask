8616cdb7af3cbd870c80664c78ef8fa6e9c4667f
ZengChao <42760076+Neekky@users.noreply.github.com>
2024-01-08 17:25:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 import sys
 sys.path.append('/usr/src/stock_analyse_tool_back_end_flask')
 from app import create_app
-from flask import Flask, request
-import pandas as pd
-import akshare as ak
-import datetime
-from config import root_path
-
-singleToday = datetime.datetime.now().strftime("%Y%m%d")
 
 flask_app = create_app()
 
