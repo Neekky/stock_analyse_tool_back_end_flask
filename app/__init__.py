0273d9914ee5a6dc6ba77f854b6abe454a8ea59c
ZengChao <42760076+Neekky@users.noreply.github.com>
2024-01-08 16:18:05 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,6 @@ def create_app(config_class=Config):
     
     # 注册蓝图
     app.register_blueprint(main_bp)
-    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(stock_data_bp, url_prefix='/auth')
     
     return app
