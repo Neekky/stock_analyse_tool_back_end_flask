6289671660bd882855cd6cdcfbbaa583d93c5d99
ZengChao <42760076+Neekky@users.noreply.github.com>
2024-01-09 16:41:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,5 @@ _ = os.path.abspath(os.path.dirname(__file__))  # 返回当前文件路径
 
 root_path = os.path.abspath(os.path.join(_, '../'))  # 返回根目录文件夹
 
-# class Config(object):
-    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    #     'sqlite:///' + os.path.join(basedir, 'app.db')
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = 'mysql://username:password@server/db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
