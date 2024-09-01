9d76754a3c114c3e589cf363f63519ef94459e59
cengchao <475383200@qq.com>
2024-09-01 21:15:59 +0800

    1.功能新增

--- update.py
+++ update.py
@@ -1,17 +1,14 @@
 import sys
 import pandas as pd
 import os
+sys.path.append('/usr/src/stock_analyse_tool_back_end_flask')
+import datetime
 
 from app.utils.trend_analysis import batching_entry
 
-import datetime
 
-sys.path.append('/usr/src/stock_analyse_tool_back_end_flask')
 # 获取当前脚本的目录
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# 在当前脚本的目录的上一级添加到 sys.path
-sys.path.append(os.path.abspath(os.path.join(current_dir, '../..')))
 
 singleToday = datetime.datetime.now().strftime("%Y%m%d")
 
@@ -21,7 +18,7 @@ resdf = batching_entry('index', 'sh000001', '2018-01-01', singleToday)
 _ = os.path.abspath(os.path.dirname(__file__))  # 返回当前文件路径
 
 # 定义数据存储路径
-database_root_path = os.path.abspath(os.path.join(_, '../../database/other'))
+database_root_path = os.path.abspath(os.path.join(_, './database/other'))
 
 # 校验 resdf 是否是一个有效的 DataFrame 且有数据
 if isinstance(resdf, pd.DataFrame) and not resdf.empty:
