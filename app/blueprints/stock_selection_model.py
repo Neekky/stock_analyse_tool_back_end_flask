3fe3bf63df4447e418ddeea6676d7f1d24dd8f3a
cengchao <475383200@qq.com>
2024-09-04 23:04:09 +0800

    1.功能新增

--- app/blueprints/stock_selection_model.py
+++ app/blueprints/stock_selection_model.py
@@ -49,6 +49,37 @@ def get_limit_kdj_model_data():
         }
         return response
 
+# 获取涨停的概念龙头数据
+@stock_selection_model_bp.route('/get_limit_leading_model_data', methods=['GET'])
+def get_limit_leading_model_data():
+    try:
+        date = request.args.get("date") or singleToday
+        # 根据日期，找到对应的数据进行返回
+        base_path = root_path + '/stock_analyse_tool_data_crawl/database/自研问句/' + date + '/涨停龙头.csv'
+
+        df = pd.read_csv(base_path)
+
+        result = df.to_json(orient="records", force_ascii=False)
+
+        # 处理数据
+        cleaned_data = clean_json(result)
+
+        cleaned_data = remove_field_from_objects(cleaned_data, '涨停明细数据')
+
+        response = {
+            'code': 200,
+            'data': cleaned_data,
+            'msg': '请求成功'
+        }
+        return response
+    except Exception as e:
+        response = {
+            'code': 500,
+            'data': [],
+            'msg': '请求发生错误'
+        }
+        return response
+
 # 获取见底的etf数据
 @stock_selection_model_bp.route('/get_etf_data', methods=['GET'])
 def get_bottom_etf_model_data():
